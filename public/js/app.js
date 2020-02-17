08c19215f8299f900d52d7f5655cc02bcaa99c5b
Skye-LAB <walksky452@gmail.com>
2020-02-17 13:42:24 +0700

    V0.0.1.1(B[resource.dupl])

--- public/js/app.js
+++ public/js/app.js
@@ -37417,7 +37417,7 @@ function normalizeComponent (
       // for template-only hot-reload because in that case the render fn doesn't
       // go through the normalizer
       options._injectStyles = hook
-      // register for functional component in vue file
+      // register for functioal component in vue file
       var originalRender = options.render
       options.render = function renderWithStyleInjection (h, context) {
         hook.call(context)
@@ -49601,7 +49601,7 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
   null,
   null,
   null
-  
+
 )
 
 /* hot reload */
@@ -49621,7 +49621,7 @@ component.options.__file = "resources/js/components/ExampleComponent.vue"
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ExampleComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./ExampleComponent.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/ExampleComponent.vue?vue&type=script&lang=js&");
-/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ExampleComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ExampleComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]);
 
 /***/ }),
 
@@ -49661,8 +49661,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\Proj\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! F:\Proj\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Skywind\Music\Fuu\This\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Skywind\Music\Fuu\This\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
---
+++ resources/views/layouts/appp.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title> @yield('title') </title>
+    @yield('css')
+    <link rel="stylesheet" href="{{ asset('/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/Footer-Basic.css') }}">
+    <link rel="stylesheet" href="{{ asset('/fonts/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/Navigation-with-Search.css') }}">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md navigation-clean-search">
+        <div class="container"><a class="navbar-brand" href="{{url('/')}}">Foody</a><button data-toggle="collapse"
+                class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span
+                    class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item mx-1" role="presentation"><a class="nav-link" href="#">
+                            <!--<span class="badge badge-danger badge-counter">3+</span>-->
+                            <ion-icon name="cart"></ion-icon>
+                        </a></li>
+                </ul>
+                <form class="form-inline mr-auto" target="_self">
+                    <div class="form-group"><span for="search-field">
+                            <ion-icon name="search"></ion-icon>
+                        </span><input class="form-control search-field" type="search" id="search-field" name="search">
+                    </div>
+                </form>
+
+                {{-- @auth
+                @else
+                @endauth --}}
+                @guest
+                  <a class="btn btn-light action-button mr-1" role="button" href="#" data-toggle="modal"
+                  data-target="#signUp">Sign Up</a>
+                  <a class="btn btn-light action-button" role="button" href="#" data-target="#signIn"
+                  data-toggle="modal">Sign in</a>
+                @else
+                  <ul class="nav navbar-nav ml-auto">
+                    <li class="dropdown nav-item"><a class="dropdown-toggle nav-link" data-toggle="dropdown"
+                      aria-expanded="false" href="#">{{auth()->user()->username}}</a>
+                      <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation"
+                        href="#">Change Password</a><a class="dropdown-item" role="presentation"
+                        href="#">Logout</a></div>
+                      </li>
+                    </ul>
+                @endguest
+            </div>
+        </div>
+    </nav>
+    <div class="modal fade" role="dialog" tabindex="-1" id="signIn">
+        <div class="modal-dialog" role="document">
+            <form action="{{ route('login') }}" method="post">
+                @csrf
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Sign In</h4><button type="button" class="close" data-dismiss="modal"
+                            aria-label="Close"><span aria-hidden="true">×</span></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="input-group mb-2">
+                            <div class="input-group-prepend"><span class="input-group-text">
+                                    <ion-icon name="mail"></ion-icon>
+                                </span></div><input class="form-control" type="text" placeholder="Email"
+                                name="email">
+                            <div class="input-group-append"></div>
+                        </div>
+                        <div class="input-group">
+                            <div class="input-group-prepend"><span class="input-group-text">
+                                    <ion-icon name="lock-closed"></ion-icon>
+                                </span></div><input class="form-control" type="password" placeholder="Password"
+                                name="password">
+                            <div class="input-group-append"></div>
+                        </div>
+                        <div class="modal-footer"><button class="btn btn-primary" type="submit">Submit</button></div>
+                    </div>
+            </form>
+        </div>
+    </div>
+    </div>
+    <div class="modal fade" role="dialog" tabindex="-1" id="signUp">
+        <div class="modal-dialog" role="document">
+            <form action="{{ route('create') }}" method="post">
+                @csrf
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Sign Up</h4><button type="button" class="close" data-dismiss="modal"
+                            aria-label="Close"><span aria-hidden="true">×</span></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="input-group mb-2">
+                            <div class="input-group-prepend"><span class="input-group-text">
+                                    <ion-icon name="person"></ion-icon>
+                                </span></div><input class="form-control" type="text" placeholder="Username"
+                                name="username">
+                            <div class="input-group-append"></div>
+                        </div>
+                        <div class="input-group mb-2">
+                            <div class="input-group-prepend"><span class="input-group-text">
+                                    <ion-icon name="mail"></ion-icon>
+                                </span></div><input class="form-control" type="text" placeholder="Email" name="email">
+                            <div class="input-group-append"></div>
+                        </div>
+                        <div class="input-group mb-2">
+                            <div class="input-group-prepend"><span class="input-group-text">
+                                    <ion-icon name="lock-closed"></ion-icon>
+                                </span></div><input class="form-control" type="password" placeholder="Password"
+                                name="password">
+                            <div class="input-group-append"></div>
+                        </div>
+                        <div class="input-group">
+                            <div class="input-group-prepend"><span class="input-group-text">
+                                    <ion-icon name="call"></ion-icon>
+                                </span></div><input class="form-control" type="text" placeholder="No. HP" name="hp">
+                            <div class="input-group-append"></div>
+                        </div>
+                    </div>
+                    <div class="modal-footer"><button class="btn btn-primary" type="submit">Submit</button></div>
+                </div>
+        </div>
+        </form>
+    </div>
+    </div>
+    </div>
+    @yield('isi')
+    <div class="footer-basic">
+        <footer>
+            <p class="copyright">Foody © 2020</p>
+        </footer>
+    </div>
+
+    @yield('javascript')
+    <script src="{{ asset('/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+</body>
+
+</html>
