e8a3c00d6fcb55d3c637796222b32c742ccad0fc
Skye-LAB <walksky452@gmail.com>
2020-02-18 08:37:31 +0700

    Truely(B)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+@section('title', 'Foody | Food Daily')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+@section('isi')
+    <div class="container">
+        <div class="row col">
+        @foreach($menu as $m)
+        <div class="card col-lg-5 col-md-6 col-sm-12 mr-2 mb-4 mx-auto" >
+            <div class="row no-gutters">
+                <div class="col-md-4 my-auto">
+                    <img src="https://pizzahutid.s3-ap-southeast-1.amazonaws.com/menu/PHD_Mybox_20190904.png" class="card-img">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$m->nama_menu}}</h5>
+                        <h6 class="text-muted card-subtitle mb-2">Price: {{$m->harga}}</h6>
+                        <button class="btn btn-outline-primary">Order</button>
+                    </div>
                 </div>
             </div>
         </div>
+        @endforeach
+        </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,63 +18,53 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/Footer-Basic.css') }}">
+    <link rel="stylesheet" href="{{ asset('/fonts/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/Navigation-with-Search.css') }}">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+      <nav class="navbar navbar-light navbar-expand-md navigation-clean-search">
+          <div class="container"><a class="navbar-brand" href="{{url('/')}}">Foody</a><button data-toggle="collapse"
+                  class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span
+                      class="navbar-toggler-icon"></span></button>
+              <div class="collapse navbar-collapse" id="navcol-1">
+                  <ul class="nav navbar-nav">
+                      <li class="nav-item mx-1" role="presentation"><a class="nav-link" href="#">
+                              <!--<span class="badge badge-danger badge-counter">3+</span>-->
+                              <ion-icon name="cart"></ion-icon>
+                          </a></li>
+                  </ul>
+                  <form class="form-inline mr-auto" target="_self">
+                      <div class="form-group"><span for="search-field">
+                              <ion-icon name="search"></ion-icon>
+                          </span><input class="form-control search-field" type="search" id="search-field" name="search">
+                      </div>
+                  </form>
+                  @guest
+                    <a class="btn btn-light action-button mr-1" role="button" href="#" data-toggle="modal"
+                    data-target="#signUp">Sign Up</a>
+                    <a class="btn btn-light action-button" role="button" href="#" data-target="#signIn"
+                    data-toggle="modal">Sign in</a>
+                  @else
+                    <ul class="nav navbar-nav ml-auto">
+                      <li class="dropdown nav-item"><a class="dropdown-toggle nav-link" data-toggle="dropdown"
+                        aria-expanded="false" href="#">{{auth()->user()->username}}</a>
+                        <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation"
+                          href="#">Change Password</a><a class="dropdown-item" role="presentation"
+                          href="#">Logout</a></div>
+                        </li>
+                      </ul>
+                  @endguest
+              </div>
+          </div>
+      </nav>
 
         <main class="py-4">
-            @yield('content')
+            @yield('isi')
         </main>
     </div>
 </body>
+<script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
 </html>
