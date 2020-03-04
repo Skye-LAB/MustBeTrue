8d95251540e3baefdbaa0ceb1bbede4bb53c8915
Skye-LAB <walksky452@gmail.com>
2020-03-04 11:48:58 +0700

    kasir(B)

--- resources/views/cashier.blade.php
+++ resources/views/cashier.blade.php
@@ -2,6 +2,45 @@
 
 @section('title', 'Cashier | Foody')
 
-@section('name')
-
+@section('isi')
+          <div class="container">
+              <div class="row mt-4">
+                  <div class="col">
+                    <div class="row">
+                      <div class="col-lg-4 mx-auto">
+                        <div class="input-group mb-3">
+                          <div class="input-group-prepend ">
+                            <label class="input-group-text" for="inputGroupSelect01">Order ID</label>
+                          </div>
+                          <select class="custom-select" id="inputGroupSelect01">
+                            <option selected>Choose...</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                      <div class="table-responsive">
+                          <table class="table mt-4">
+                              <thead>
+                                  <tr>
+                                      <th>No.</th>
+                                      <th>Nama Menu</th>
+                                      <th>Qty</th>
+                                      <th>Harga</th>
+                                  </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                </tr>
+                              </tbody>
+                          </table>
+                      </div>
+                      <center>
+                        <button class="btn btn-primary mx-auto" type="submit">Save</button>
+                      </center>
+                  </div>
+              </div>
+          </div>
 @endsection
